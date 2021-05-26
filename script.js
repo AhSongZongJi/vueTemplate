97d3df1dbb899a960d75fb74fbe88209bcba796e
songlei01 <songlei01@beantechs.cn>
2021-05-26 11:20:18 +0800

    添加注释 优化代码

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const childProcess = require('child_process');
 
 const env = process.env.NODE_ENV;
-
+//  "build" "cross-env NODE_ENV=test node script.js", 针对于运维只执行 npm run build 脚本 发布版本 不跟对应的环境脚本需要自己处理
 childProcess.execSync(`cross-env NODE_ENV=production VUE_APP_ENV=${env} vue-cli-service build`);
--- src/main.js
+++ src/main.js
@@ -7,15 +7,13 @@ Vue.config.productionTip = false;
 // 1 jenkins 发布
 // --------package.json  脚本命令------------
 //  cross-env  允许往全局注入变量
-//  如果运维不是根据不同的命令打包 只注入环境变量 可以根据全局process.env.NODE_ENV获取
-// "build": "cross-env NODE_ENV=test node script.js",
-//   根据运维不通的打包命令 去读取不同的文件  然后全局注入变量 可以访问全局变量process.env.自定义的变量名称获取
+//  如果运维是根据相同的命令打包 只注入环境变量  可以根据当前注入的环境变量 重新自定义一个环境变量
+// "build": "cross-env NODE_ENV=test  node script.js",
 
-// cross-env NODE_ENV=production
-// NODE_ENV = production vue 打包机制才会认为是生产模式  才会代码压缩 路由hash 加时间戳  这样发布各个版本 不会有缓存
+//  根据运维不同环境脚本的打包命令 去读取对应环境的脚本  然后全局注入变量 可以访问全局变量process.env.NODE_ENV获取
 
-// "build:test": "NODE_ENV=production vue-cli-service build --mode test",
-// "build:prod": "NODE_ENV=production vue-cli-service build --mode prod",
+// "build:test": "NODE_ENV=production vue-cli-service build --mode test",    /* 可以把--mode 去了 */
+// "build:prod": "NODE_ENV=production vue-cli-service build --mode prod",   /* 可以把--mode 去了 */
 // "lint": "vue-cli-service lint --fix"
 new Vue({
   router,
